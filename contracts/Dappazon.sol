426cbc3a5ea1e4c6d2f60ab097228edebb733ee4
tzafff <christzaf99@gmail.com>
2024-02-27 21:36:12 +0200

    Set Contract

--- contracts/Dappazon.sol
+++ contracts/Dappazon.sol
@@ -1,4 +1,14 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract Dappazon {}
+contract Dappazon {
+
+    string public name;
+    address public owner;
+
+
+    constructor() {
+        name = "Dappazon";
+        owner = msg.sender;
+    }
+}
--- test/Dappazon.js
+++ test/Dappazon.js
@@ -1,9 +1,32 @@
-const { expect } = require("chai")
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
 const tokens = (n) => {
-  return ethers.utils.parseUnits(n.toString(), 'ether')
-}
+  return ethers.utils.parseUnits(n.toString(), "ether");
+};
 
 describe("Dappazon", () => {
 
-})
+  let dappazon;
+  let deployer,  buyer;
+
+  beforeEach(async () => {
+
+    // Fetch Accounts
+
+    [deployer, buyer] = await ethers.getSigners()
+
+    // Deploy Contract
+    const Dappazon = await ethers.getContractFactory("Dappazon");
+    dappazon = await Dappazon.deploy();
+
+    
+  });
+
+  describe("Deployment", () => {
+
+    it("Sets the owner", async () => {
+      expect(await dappazon.owner()).to.be.equal(deployer.address);
+    });
+  });
+});

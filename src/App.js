ed6433bd505bbcb7cfa7f6e40beb9132446b158a
tzafff <christzaf99@gmail.com>
2024-03-06 00:30:06 +0200

    Product Details

--- src/App.js
+++ src/App.js
@@ -1,87 +1,102 @@
-import { useEffect, useState } from 'react'
-import { ethers } from 'ethers'
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
 
 // Components
-import Navigation from './components/Navigation'
-import Section from './components/Section'
-import Product from './components/Product'
+import Navigation from "./components/Navigation";
+import Section from "./components/Section";
+import Product from "./components/Product";
 
 // ABIs
-import Dappazon from './abis/Dappazon.json'
+import Dappazon from "./abis/Dappazon.json";
 
 // Config
-import config from './config.json'
+import config from "./config.json";
 
 function App() {
+  const [account, setAccount] = useState(null);
+  const [provider, setProvider] = useState(null);
+  const [dappazon, setDappazon] = useState(null);
 
-  const [account, setAccount] = useState(null)
-  const [provider, setProvider] = useState(null)
-  const [dappazon, setDappazon] = useState(null)
+  const [electronics, setElectronics] = useState(null);
+  const [clothing, setClothing] = useState(null);
+  const [toys, setToys] = useState(null);
 
-  const [electronics, setElectronics] = useState(null)
-  const [clothing, setClothing] = useState(null)
-  const [toys, setToys] = useState(null)
-
-  const [toggle, setToggle] = useState(false)
+  const [toggle, setToggle] = useState(false);
+  const [item, setItem] = useState({});
 
   const togglePop = (item) => {
-    console.log("toggle")
-  }
+    setItem(item);
+    console.log(toggle)
+    toggle ? setToggle(false) : setToggle(true);
+  };
 
   const loadBlockchainData = async () => {
     // Connect to blockchain
-    const provider = new ethers.providers.Web3Provider(window.ethereum)
-    setProvider(provider)
-    
-    const network = await provider.getNetwork()
-  
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    setProvider(provider);
+
+    const network = await provider.getNetwork();
+
     // Connect to Contract
-    const dappazon = new ethers.Contract ( config[network.chainId].dappazon.address, Dappazon, provider)
-    
-    setDappazon(dappazon)
+    const dappazon = new ethers.Contract(
+      config[network.chainId].dappazon.address,
+      Dappazon,
+      provider
+    );
+
+    setDappazon(dappazon);
 
     // Load products
 
-    const items = []
-    for(var i = 0; i < 9; i ++) {
+    const items = [];
+    for (var i = 0; i < 9; i++) {
       const item = await dappazon.items(i + 1);
-      items.push(item)
+      items.push(item);
     }
-    console.log(items)
-    
-    const electronics = items.filter((item) => item.category === 'electronics')
-    const clothing = items.filter((item) => item.category === 'clothing')
-    const toys = items.filter((item) => item.category === 'toys')
 
-    setElectronics(electronics)
-    setClothing(clothing)
-    setToys(toys)
-  }
+    const electronics = items.filter((item) => item.category === "electronics");
+    const clothing = items.filter((item) => item.category === "clothing");
+    const toys = items.filter((item) => item.category === "toys");
+
+    setElectronics(electronics);
+    setClothing(clothing);
+    setToys(toys);
+  };
 
   useEffect(() => {
-    loadBlockchainData()
+    loadBlockchainData();
 
-    window.ethereum.on('accountsChanged', (accounts) => {
-      const newAccount = ethers.utils.getAddress(accounts[0])
-      setAccount(newAccount)
-      console.log('Account changed:', newAccount);
-    })
-  },[])
+    window.ethereum.on("accountsChanged", (accounts) => {
+      const newAccount = ethers.utils.getAddress(accounts[0]);
+      setAccount(newAccount);
+      console.log("Account changed:", newAccount);
+    });
+  }, []);
 
   return (
     <div>
       <Navigation account={account} setAccount={setAccount} />
       <h2>Dappazon Best Sellers</h2>
 
-        
-    {electronics && clothing && toys && (
-       <>
-        <Section title={"Clothing & Jewelry"} items={clothing} togglePop={togglePop} />
-        <Section title={"Electronics & Gadgets"} items={electronics} togglePop={togglePop} />
-        <Section title={"Toys & Gaming"} items={toys} togglePop={togglePop} />
-     </>
-    )}
+      {electronics && clothing && toys && (
+        <>
+          <Section
+            title={"Clothing & Jewelry"}
+            items={clothing}
+            togglePop={togglePop}
+          />
+          <Section
+            title={"Electronics & Gadgets"}
+            items={electronics}
+            togglePop={togglePop}
+          />
+          <Section title={"Toys & Gaming"} items={toys} togglePop={togglePop} />
+        </>
+      )}
 
+      {toggle && (
+        <Product item={item} provider={provider} account={account} dappazon={dappazon} togglePop={togglePop} />
+      )}
     </div>
   );
 }
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,18 +1,94 @@
-import { useEffect, useState } from 'react'
-import { ethers } from 'ethers'
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
 
 // Components
-import Rating from './Rating'
+import Rating from "./Rating";
 
-import close from '../assets/close.svg'
+import close from "../assets/close.svg";
 
 const Product = ({ item, provider, account, dappazon, togglePop }) => {
+  const [order, setOrder] = useState(null)
+  
+  const buyHandler = async () => {
+    console.log("handler")
+  }
 
   return (
     <div className="product">
+      <div className="product__details">
+        <div className="product__image">
+          <img src={item.image} alt="Product" />
+        </div>
+        <div className="product__overview">
+          <h1>{item.name}</h1>
 
-    </div >
+          <Rating value={item.rating} />
+
+          <hr />
+
+          <p>{item.address}</p>
+
+          <h2>{ethers.utils.formatUnits(item.cost.toString(), "ether")} ETH</h2>
+
+          <hr />
+
+          <h2>Overview</h2>
+
+          <p>
+            {item.description}
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima rem,
+            iusto, consectetur inventore quod soluta quos qui assumenda aperiam,
+            eveniet doloribus commodi error modi eaque! Iure repudiandae
+            temporibus ex? Optio!
+          </p>
+        </div>
+
+        <div className="product__order">
+          <h1>{ethers.utils.formatUnits(item.cost.toString(), "ether")} ETH</h1>
+          <p>
+            FREE delivery <br />
+            <strong>
+              {new Date(Date.now() + 345600000).toLocaleDateString(undefined, {
+                weekday: "long",
+                month: "long",
+                day: "numeric",
+              })}
+            </strong>
+          </p>
+
+          {item.stock > 0 ? <p>In Stock.</p> : <p>Out of Stock.</p>}
+
+          <button className="product__buy" onClick={buyHandler}>
+            Buy Now
+          </button>
+
+          <p><small>Ships from</small> Dappazon</p>
+          <p><small>Sold by</small> Dappazon</p>
+
+          {order && (
+            <div className='product__bought'>
+              Item bought on <br />
+              <strong>
+                {new Date(Number(order.time.toString() + '000')).toLocaleDateString(
+                  undefined,
+                  {
+                    weekday: 'long',
+                    hour: 'numeric',
+                    minute: 'numeric',
+                    second: 'numeric'
+                  })}
+              </strong>
+            </div>
+          )}
+          
+        </div>
+
+        <button onClick={togglePop} className="product__close">
+          <img src={close} alt="Close" />
+        </button>
+      </div>
+    </div>
   );
-}
+};
 
 export default Product;

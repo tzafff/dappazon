899c0cc5ff07c321e3e8d3ea31acc87da8eeb73e
tzafff <christzaf99@gmail.com>
2024-03-06 00:08:33 +0200

    List Products

--- src/App.js
+++ src/App.js
@@ -15,9 +15,47 @@ import config from './config.json'
 function App() {
 
   const [account, setAccount] = useState(null)
+  const [provider, setProvider] = useState(null)
+  const [dappazon, setDappazon] = useState(null)
+
+  const [electronics, setElectronics] = useState(null)
+  const [clothing, setClothing] = useState(null)
+  const [toys, setToys] = useState(null)
+
+  const [toggle, setToggle] = useState(false)
+
+  const togglePop = (item) => {
+    console.log("toggle")
+  }
 
   const loadBlockchainData = async () => {
+    // Connect to blockchain
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    setProvider(provider)
+    
+    const network = await provider.getNetwork()
   
+    // Connect to Contract
+    const dappazon = new ethers.Contract ( config[network.chainId].dappazon.address, Dappazon, provider)
+    
+    setDappazon(dappazon)
+
+    // Load products
+
+    const items = []
+    for(var i = 0; i < 9; i ++) {
+      const item = await dappazon.items(i + 1);
+      items.push(item)
+    }
+    console.log(items)
+    
+    const electronics = items.filter((item) => item.category === 'electronics')
+    const clothing = items.filter((item) => item.category === 'clothing')
+    const toys = items.filter((item) => item.category === 'toys')
+
+    setElectronics(electronics)
+    setClothing(clothing)
+    setToys(toys)
   }
 
   useEffect(() => {
@@ -33,7 +71,16 @@ function App() {
   return (
     <div>
       <Navigation account={account} setAccount={setAccount} />
-      <h2>Welcome to Dappazon</h2>
+      <h2>Dappazon Best Sellers</h2>
+
+        
+    {electronics && clothing && toys && (
+       <>
+        <Section title={"Clothing & Jewelry"} items={clothing} togglePop={togglePop} />
+        <Section title={"Electronics & Gadgets"} items={electronics} togglePop={togglePop} />
+        <Section title={"Toys & Gaming"} items={toys} togglePop={togglePop} />
+     </>
+    )}
 
     </div>
   );
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,14 +1,34 @@
-import { ethers } from 'ethers'
+import { ethers } from "ethers";
 
 // Components
-import Rating from './Rating'
+import Rating from "./Rating";
 
 const Section = ({ title, items, togglePop }) => {
-    return (
-        <div className='cards__section'>
+  console.log(items.image);
+  return (
+    <div className="cards__section">
+      <h3>{title}</h3>
 
-        </div>
-    );
-}
+      <hr />
+
+      <div className="cards">
+        {items.map((item, index) => (
+          <div className="card" key={index} onClick={() => togglePop(item)}>
+            <div>
+              <img src={item.image} alt="Item" />
+            </div>
+            <div className="card__info">
+              <h4>{item.name}</h4>
+              <Rating value={item.rating} />
+              <p>
+                {ethers.utils.formatUnits(item.cost.toString(), "ether")} ETH
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Section;

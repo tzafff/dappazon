b75bf6f0ad25dd193834776b73ad5c0661a858bc
tzafff <christzaf99@gmail.com>
2024-03-06 19:19:07 +0200

    Buy functions

--- src/components/Product.js
+++ src/components/Product.js
@@ -8,10 +8,32 @@ import close from "../assets/close.svg";
 
 const Product = ({ item, provider, account, dappazon, togglePop }) => {
   const [order, setOrder] = useState(null)
+  const [hasBought, setHasBought] = useState(false)
   
-  const buyHandler = async () => {
-    console.log("handler")
+
+  const fetchDetails = async () => {
+    const events = await dappazon.queryFilter("Buy")
+    const orders = events.filter(
+      (event) => event.args.buyer === account && event.args.itemId.toString() === item.id.toString()
+    )
+
+    if(orders.length === 0 ) return
+
+    const order = await dappazon.orders(account, orders[0].args.orderId)
+    setOrder(order)
   }
+
+  const buyHandler = async () => {
+    const signer = await provider.getSigner();
+    let transaction = dappazon.connect(signer).buy(item.id, {value: item.cost})
+    await transaction.wait()
+
+    setHasBought(true)
+  }
+
+  useEffect(() => {
+    fetchDetails()
+  },[hasBought])
 
   return (
     <div className="product">
